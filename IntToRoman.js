53e18efbfe3d5e96990c3a29e5109b8c8e35bcf2
Rusmia Rahaman Urfa <156609609+Rusmia-Rahaman-Urfa@users.noreply.github.com>
2025-08-16 20:04:56 +0600

    Add files via upload

---
+++ IntToRoman.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const valueSymbols = [
+      [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+      [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+      [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+    ];
+    let result = '';
+
+    for (const [value, symbol] of valueSymbols) {
+      if (num === 0) {
+        break;
+      }
+      
+      while (num >= value) {
+        result += symbol;
+        num -= value;
+      }
+    }
+
+    return result;    
+};
+
+// Example usage to get output
+console.log(intToRoman(3749));     // Output: "MMMDCCXLIX"
+console.log(intToRoman(58));    // Output: "LVIII"
+console.log(intToRoman(1994));  // Output: "MCMXCIV"
