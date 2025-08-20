5209e2ebc901927c94b7e8c59ffce8f4ea234ff4
Rusmia Rahaman Urfa <156609609+Rusmia-Rahaman-Urfa@users.noreply.github.com>
2025-08-20 23:58:27 +0600

    Add files via upload

---
+++ LongestSubstringWihoutRepeatingCharacters.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let left = 0;
+    let maxLength = 0;
+    let charSet = new Set();
+
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++;
+        }
+
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;    
+};
+
+// Example usage to get results
+console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
+console.log(lengthOfLongestSubstring("bbbbb"));    // Output: 1
+console.log(lengthOfLongestSubstring("pwwkew"));   // Output: 3
